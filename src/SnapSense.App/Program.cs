c28d0ae150d6a5b33e86bfd33c210fd9eb12108a
Jean Farrugia <jeanfrg@gmail.com>
2024-07-27 17:12:45 +0200

    chore: formatting

--- src/SnapSense.App/Program.cs
+++ src/SnapSense.App/Program.cs
@@ -6,6 +6,7 @@ internal static class Program
     {
         var cancellationTokenSource = new CancellationTokenSource();
         var cancellationToken = cancellationTokenSource.Token;
+
         using (var webcam = new WebcamHandler(new []{ new FacePersistenceFrameHandler(new FaceDetector(), cancellationTokenSource) }))
         {
             webcam.Start(cancellationToken);
